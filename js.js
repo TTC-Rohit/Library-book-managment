b557df38515bd5df68d5a83e452665ca941336a1
Rohit Suthar <rohit@taldartech.in>
2026-07-30 18:12:54 +0530

    fix bugs and add statistics

--- js.js
+++ js.js
@@ -1,51 +1,103 @@
 let books = [
-  //   {
-  //     bookId: "8342",
-  //     author: "Andy Weir",
-  //     category: "sci-fi",
-  //     title: "The Martian",
-  //     available: "available",
-  //   },
-  //   {
-  //     bookId: "1928",
-  //     author: "J.R.R. Tolkien",
-  //     category: "fantasy",
-  //     title: "The Fellowship of the Ring",
-  //     available: "issued",
-  //   },
-  //   {
-  //     bookId: "574",
-  //     author: "James Clear",
-  //     category: "self-help",
-  //     title: "Atomic Habits",
-  //     available: "available",
-  //   },
-  //   {
-  //     bookId: "9102",
-  //     author: "Alan Moore",
-  //     category: "comic",
-  //     title: "Watchmen",
-  //     available: "available",
-  //   },
-  //   {
-  //     bookId: "4431",
-  //     author: "Yuval Noah Harari",
-  //     category: "history",
-  //     title: "Sapiens: A Brief History of Humankind",
-  //     available: "issued",
-  //   },
+{
+  bookId: "1001",
+  author: "George R. R. Martin",
+  category: "fantasy",
+  title: "A Game of Thrones",
+  available: "available",
+},
+{
+  bookId: "1002",
+  author: "Patrick Rothfuss",
+  category: "fantasy",
+  title: "The Name of the Wind",
+  available: "issued",
+},
+{
+  bookId: "1003",
+  author: "Neil Gaiman",
+  category: "comic",
+  title: "The Sandman",
+  available: "available",
+},
+{
+  bookId: "1004",
+  author: "Frank Miller",
+  category: "comic",
+  title: "The Dark Knight Returns",
+  available: "issued",
+},
+{
+  bookId: "1005",
+  author: "Stephen E. Ambrose",
+  category: "history",
+  title: "Band of Brothers",
+  available: "available",
+},
+{
+  bookId: "1006",
+  author: "Doris Kearns Goodwin",
+  category: "history",
+  title: "Team of Rivals",
+  available: "issued",
+},
+{
+  bookId: "1007",
+  author: "Isaac Asimov",
+  category: "sci-fi",
+  title: "Foundation",
+  available: "available",
+},
+{
+  bookId: "1008",
+  author: "Frank Herbert",
+  category: "sci-fi",
+  title: "Dune",
+  available: "issued",
+},
+{
+  bookId: "1009",
+  author: "Robin Sharma",
+  category: "self-help",
+  title: "The Monk Who Sold His Ferrari",
+  available: "available",
+},
+{
+  bookId: "1010",
+  author: "Mark Manson",
+  category: "self-help",
+  title: "The Subtle Art of Not Giving a F*ck",
+  available: "issued",
+},
+{
+  bookId: "1011",
+  author: "Paulo Coelho",
+  category: "other",
+  title: "The Alchemist",
+  available: "available",
+},
+{
+  bookId: "1012",
+  author: "Khaled Hosseini",
+  category: "other",
+  title: "The Kite Runner",
+  available: "issued",
+}
 ]
-  
+// for enter demo data 
 // localStorage.setItem("books", JSON.stringify(books));
+
 let loStor = JSON.parse(localStorage.getItem("books"));
 if (loStor != null) {
   books = loStor;
 }
 let filterdBooks = [];
+// filter books selected category
 function filterBooks(cate) {
   filterdBooks = books.filter((book) => book.category.toLowerCase() == cate);
   showBooks();
 }
+states();
 function showBooks(key = "") {
   $(".cards").empty();
   let searchList = [];
@@ -55,6 +107,7 @@ function showBooks(key = "") {
     searchList = books;
   }
   searchList.forEach((book) => {
+
     const card = `<div class="card p-3 mx-auto my-3 shadow-sm" style="width: 300px; height: 300px;">
     <div class="d-flex justify-content-between border-bottom pb-2 mb-3">
         <p class="mb-0 text-truncate " style="width:80%"><strong class="title">${book.title} </strong></p>
@@ -83,11 +136,13 @@ function showBooks(key = "") {
   });
 }
 showBooks();
+
 function dltBook(btn) {
   $(btn).closest(".card").remove();
   const id = $(btn).closest(".card").find(".bookId").text();
   books = books.filter((book) => book.bookId != id);
   localStorage.setItem("books", JSON.stringify(books));
+  states();
 }
 
 function editSave(btn) {
@@ -95,16 +150,17 @@ function editSave(btn) {
   $card = $(btn).closest(".card");
   let id = $card.find(".bookId").text();
   let title = $card.find(".title").text();
-  let category = $card.find(".category").text();
+  let category = $card.find(".category").text().trim();  
   let available = $card.find(".available").text();
   let author = $card.find(".author").text();
+
   //edit form
   if ($(btn).text() == "Edit") {
     const card = `
 <form action="submit" class="form"> 
     <div class="d-flex justify-content-between border-bottom pb-2 mb-3">
         <p class="mb-0 text-truncate " style="width:80%">
-            <input type="text" class="title p-0" value="${title}" required style="width:90%">
+            <input type="text" class="title p-0" value="${title}" placeholder="title here" required style="width:90%">
         </p>
      <p class="mb-0  text-success available">
         <select value="${available}" class="p-0" required>
@@ -126,7 +182,7 @@ ${
     <div class="details">
         <p class="mb-1">BookId :
              <strong class="bookId ">${id}</strong>
-        <p class="mb-1">Author : <input class="author p-0" type="text" value="${author}" required></p>
+        <p class="mb-1">Author : <input class="author p-0" type="text" placeholder="author" value="${author}" required></p>
        <p class="mb-0">Category :
          <select value="${category}" class="p-0 category" required>
 ${
@@ -135,9 +191,9 @@ ${
     : `<option value="fantasy">fantasy</option>`
 }
 ${
-  category == "comisc"
-    ? `<option value="comisc" selected>comisc</option>`
-    : `<option value="comisc">comisc</option>`
+  category == "comic"
+    ? `<option value="comic" selected>comic</option>`
+    : `<option value="comic">comic</option>`
 }
 ${
   category == "history"
@@ -168,12 +224,15 @@ ${
     </div>
     </form>
 `;
-    $card.html(card$);
+
+
+    $card.html(card);
   } else {
 const form=$card.find("form");
     if (!$card.find(".form")[0].reportValidity()) {
         return;
     }
+  
     //save--------------
     title = $card.find(".title").val();
     category = $card.find(".category").val();
@@ -200,6 +259,7 @@ const form=$card.find("form");
       });
     }
     localStorage.setItem("books", JSON.stringify(books));
+      states();
     showBooks();
     const card = ` <div class="d-flex justify-content-between border-bottom pb-2 mb-3">
         <p class="mb-0 text-truncate " style="width:80%"><strong class="title">${title} </strong></p>
@@ -229,7 +289,7 @@ function addNew() {
 <form action="submit" class="form"> 
     <div class="d-flex justify-content-between border-bottom pb-2 mb-3">
         <p class="mb-0 text-truncate " style="width:80%">
-            <input type="text" class="title p-0" value="${title}" required style="width:90%">
+            <input type="text" class="title p-0" placeholder="title here" value="${title}" required style="width:90%">
         </p>
      <p class="mb-0  text-success available">
         <select value="${available}" required class="p-0">
@@ -250,7 +310,7 @@ ${
     <div class="details">
         <p class="mb-1">BookId :
              <strong class="bookId ">${id}</strong>
-        <p class="mb-1">Author : <input class="author p-0" type="text" value="${author}" required></p>
+        <p class="mb-1">Author : <input class="author p-0" type="text" value="${author}" placeholder="author" required></p>
          <p class="mb-0">Category :
          <select value="${category}" class="p-0 category" required>
 ${
@@ -259,9 +319,9 @@ ${
     : `<option value="fantasy">fantasy</option>`
 }
 ${
-  category == "comisc"
-    ? `<option value="comisc" selected>comisc</option>`
-    : `<option value="comisc">comisc</option>`
+  category == "comic"
+    ? `<option value="comic" selected>comic</option>`
+    : `<option value="comic">comic</option>`
 }
 ${
   category == "history"
@@ -294,4 +354,18 @@ ${
     </div>
 `;
   $(".cards").prepend(card);
+};
+function states() {
+    let list=localStorage.getItem("books")
+    let total=0;
+    let available=0;
+    let issued=0;
+    books.forEach((book)=>{
+        if(book.available=="available"){available++;}
+        else{issued++;}
+    })
+    total=available+issued;
+    $(".total").text(total);
+    $(".ava").text(available)
+    $(".issue").text(issued);;
 }
